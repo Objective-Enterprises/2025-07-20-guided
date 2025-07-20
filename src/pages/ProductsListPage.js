50884210c55d922573367bdf0949f7aa18a0a26d
David Y. Stephenson <david@davidystephenson.com>
2025-07-20 13:12:06 -0400

    Display product list

--- src/pages/ProductsListPage.js
+++ src/pages/ProductsListPage.js
@@ -1,19 +1,25 @@
 import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 import AlertMessage from "../components/AlertMessage";
 import Spinner from "react-bootstrap/Spinner";
 
 const ProductsListPage = () => {
-  const [productsList, setProductsList] = useState();
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [])
+  const productsList = useSelector(state => state.productList.products)
+  const loading = useSelector(state => state.productList.loading)
 
-  //WRITE YOUR CODE HERE
+  if (loading) {
+    return <Spinner />
+  }
+
   return (
     <>
-      {/*WRITE YOUR CODE HERE*/}
       {productsList && productsList.length === 0 && (
         <AlertMessage variant="info" message="No products to display" />
       )}
