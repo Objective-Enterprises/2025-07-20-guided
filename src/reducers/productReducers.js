0b8d653bdabad249a3dbe3dc44ee1367d08a8ebf
David Y. Stephenson <david@davidystephenson.com>
2025-07-20 12:53:25 -0400

    Define reducers

--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,9 +1,33 @@
-//WRITE YOUR CODE HERE
+import { START_PRODUCTS, END_PRODUCTS, START_PRODUCT, END_PRODUCT } from "../constants/productActionConstants"
 
 export const listProductsReducer = (state = { products: [] }, action) => {
-//WRITE YOUR CODE HERE
+  switch (action.type) {
+    case START_PRODUCTS: {
+      const newState = { ...state, loading: true, products: [] }
+      return newState
+    }
+    case END_PRODUCTS: {
+      const newState = { ...state, loading: false, products: action.payload }
+      return newState
+    }
+    default: {
+      return state
+    }
+  }
 }
 
 export const fetchProductDetailsReducer = (state = { product: {} }, action) => {
-//WRITE YOUR CODE HERE
+  switch (action.type) {
+    case START_PRODUCT: {
+      const newState = { ...state, loading: true, product: {} }
+      return newState
+    }
+    case END_PRODUCT: {
+      const newState = { ...state, loading: true, product: action.payload }
+      return newState
+    }
+    default: {
+      return state
+    }
+  }
 }
